72f0658f178ed0cdccd29c76d00248f7cbb05ee6
MathRogelin <matheusrogelin15@gmail.com>
2025-07-22 17:49:08 -0300

    Atualizando secao questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,5 +1,5 @@
 const questions = [
-    "É verdade que o personagem X já conhecia o Y antes dos eventos principais?",
+    "Diego, você já mencionou que nem todas as Diamonds são criações suas, então caso algum dos outros colaboradores da AU quiserem fazer uma comic sobre sua respectiva Diamond, isso seria possível?",
     "Por que a personagem Z parece tão fria no início? Ela mudou muito…",
     "Como funcionam os poderes nesse universo? Eles nascem com as pessoas ou são adquiridos?",
     "Se o personagem Q não tivesse se sacrificado, o final seria diferente?",
@@ -11,15 +11,7 @@ const questions = [
 ]
 
 const answers = [
-    "Essa é uma ótima teoria — eles se cruzaram brevemente quando eram crianças, mas só um deles se lembra disso conscientemente…",
-    "Ela passou por uma perda importante antes da história começar. Mostrar essa armadura emocional era essencial para o arco dela.",
-    "Nascem com elas, mas ficam dormentes até um 'evento de ativação', que varia conforme a linhagem. Essa mecânica é explorada na Parte 2!",
-    "Com certeza. O destino do grupo inteiro teria sido alterado. Mas o Q fez isso por escolha, e não se arrependeu.",
-    "Ela é paralela — os eventos são os mesmos até certo ponto, mas divergem completamente após o Capítulo 3.",
-    "Você está quente. Algumas partes foram delírios… mas uma cena específica ainda vai se repetir no futuro",
-    "Hmmm... posso dizer que eles terão a chance de escolher. Mas nem tudo depende só deles.",
-    "Porque ela passou por um recomeço interno. O novo visual representa a forma como ela se vê agora.",
-    "Ele tem traços inspirados em pessoas próximas — especialmente a coragem e a teimosia. Mas o M é 100% do universo da AU."
+    "",
 ]
 
 document.addEventListener("DOMContentLoaded", ()=>{
