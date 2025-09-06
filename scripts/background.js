a675776646deefb43896d13c5ac359749206d4c1
MathRogelin <matheusrogelin15@gmail.com>
2025-09-06 14:54:38 -0300

    Atualizando pagina

--- scripts/background.js
+++ scripts/background.js
@@ -1,27 +1,27 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const imagens = [
-    "../assets/images/bg13.png",
-    "../assets/images/bg14.png"
-  ];
+// window.addEventListener("DOMContentLoaded", () => {
+//   const imagens = [
+//     "../assets/images/bg13.png",
+//     "../assets/images/bg14.png"
+//   ];
 
-  let index = 0;
+//   let index = 0;
 
-  function trocarFundo() {
-    const proximaImagem = imagens[index];
-    const img = new Image();
+//   function trocarFundo() {
+//     const proximaImagem = imagens[index];
+//     const img = new Image();
 
-    img.onload = () => {
-      document.body.style.backgroundImage = `url('${proximaImagem}')`;
-      index = (index + 1) % imagens.length;
-    };
+//     img.onload = () => {
+//       document.body.style.backgroundImage = `url('${proximaImagem}')`;
+//       index = (index + 1) % imagens.length;
+//     };
 
-    img.onerror = () => {
-      console.error(`Erro ao carregar imagem: ${proximaImagem}`);
-    };
+//     img.onerror = () => {
+//       console.error(`Erro ao carregar imagem: ${proximaImagem}`);
+//     };
 
-    img.src = proximaImagem;
-  }
+//     img.src = proximaImagem;
+//   }
 
-  trocarFundo(); // inicia com a primeira imagem
-  setInterval(trocarFundo, 10000); // troca a cada 6 segundos
-});
+//   trocarFundo(); // inicia com a primeira imagem
+//   setInterval(trocarFundo, 10000); // troca a cada 6 segundos
+// });
