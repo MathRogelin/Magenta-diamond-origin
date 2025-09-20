dccd05935780c99b35d43231d80a46901459cfc7
MathRogelin <matheusrogelin15@gmail.com>
2025-09-20 13:17:24 -0300

    Excluido arquivos .js desnecessarios

--- scripts/seasons.js
+++
@@ -1,9 +0,0 @@
-const seasons = document.querySelectorAll(".apresentation_stories .frame_stories .frame .poster")
-
-console.log(seasons)
-
-seasons.forEach((season)=>{
-    season.addEventListener("click", ()=>{
-        window.open("seasons.html","_blank")
-    })
-})
